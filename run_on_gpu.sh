db3d3002a298053df02bd49597b1cc660e73ada4
jakkrit <jakkrit@ppsmartbot.com>
2019-10-30 00:46:44 +0700

    mv folder

--- run_on_gpu.sh
+++ run_on_gpu.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-docker run -p 8889:8889 --gpus all -it \
-    -v /home/jakkrit/Desktop/jupyter-lab-server/fastai/nbs/dl1:/projects/fastai/dl1 \
+docker run -p 8888:8888 --restart always --gpus all -it \
+    -v /home/jakkrit/Desktop/jupyter-lab-server/projects:/projects/fastai/dl1 \
     -v /home/jakkrit/Desktop/jupyter-lab-server/fastai/nbs/dl2:/projects/fastai/dl2 \
     -v /home/jakkrit/Desktop/jupyter-lab-server/notebooks:/projects/notebooks \
     -v /home/jakkrit/.gitconfig:/root/.gitconfig \
     -v /home/jakkrit/.ssh:/root/.ssh \
     -v jupyter-gpu-config:/root/.jupyter/ ppsmart/jupyterlab-torch-fastai:latest \
-    --restart=always
