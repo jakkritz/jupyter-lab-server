c90e95eadebfc7c1c84451856058333597728e80
jakkrit <jakkrit@ppsmartbot.com>
2019-10-29 21:41:01 +0700

    rev dockerfile

--- run_on_gpu.sh
+++ run_on_gpu.sh
@@ -2,7 +2,8 @@
 docker run -p 8889:8889 --gpus all -it \
     -v /home/jakkrit/Desktop/jupyter-lab-server/fastai/nbs/dl1:/projects/fastai/dl1 \
     -v /home/jakkrit/Desktop/jupyter-lab-server/fastai/nbs/dl2:/projects/fastai/dl2 \
-    -v /home/jakkrit/Desktop/jupyter-lab-server/projects/scratch:/projects/scratch \
+    -v /home/jakkrit/Desktop/jupyter-lab-server/notebooks:/projects/notebooks \
     -v /home/jakkrit/.gitconfig:/root/.gitconfig \
     -v /home/jakkrit/.ssh:/root/.ssh \
-    -v jupyter-gpu-config:/root/.jupyter/ ppsmart/jupyterlab-torch-fastai:latest
+    -v jupyter-gpu-config:/root/.jupyter/ ppsmart/jupyterlab-torch-fastai:latest \
+    --restart=always
